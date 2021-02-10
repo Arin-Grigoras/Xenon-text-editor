ecdc23b87f3fcdf91a29746a9f9f2d01f7ca444c
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-10 18:22:12 +0200

    abAppend function

--- editor/xenon.c
+++ editor/xenon.c
@@ -8,9 +8,11 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <sys/ioctl.h>
+#include <string.h>
 
 //DEFINES
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define XENON_VERSION "0.0.1"
 
 
 //DATA
@@ -109,28 +111,73 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 
+//APPEND BUFFER
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+
 
 //OUTPUT
-void editorDrawRows() {
-  int y;
-  for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~", 1);
-    if (y < E.screenrows - 1) {
-      write(STDOUT_FILENO, "\r\n", 2);
+void editorDrawRows(struct abuf *ab) {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        if (y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+            "Kilo editor -- version %s", XENON_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+            abAppend(ab, "~", 1);
+            padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+        } else {
+        abAppend(ab, "~", 1);
+        }
+        abAppend(ab, "\x1b[K", 3);
+        if (y < E.screenrows - 1) {
+        abAppend(ab, "\r\n", 2);
+        }
     }
-  }
 }
 
 
-void editorRefreshScreen(){
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
 
-    editorDrawRows();
+void editorRefreshScreen() {
+  struct abuf ab = ABUF_INIT;
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25l", 6);
+  //abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[H", 3);
+
+  editorDrawRows(&ab);
+
+  abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
-
 
 
 /*** input ***/
