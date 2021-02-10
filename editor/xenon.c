4f11122688177a07ce436cb0882ede3d8bcbc218
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-10 17:55:19 +0200

    Draw rows and refresh screen

--- editor/xenon.c
+++ editor/xenon.c
@@ -60,6 +60,25 @@ char editorReadKey() {
 }
 
 
+//OUTPUT
+void editorDrawRows(){
+    int y;
+    for(y = 0; y < 24; y++){
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+
+void editorRefreshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
 
 /*** input ***/
 void editorProcessKeypress() {
