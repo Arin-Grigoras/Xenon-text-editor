f57974e9ed326acf615a2c8341d35b8ea25b4801
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-10 17:52:49 +0200

    Just made it better in general

--- editor/xenon.c
+++ editor/xenon.c
@@ -1,40 +1,90 @@
+//INCLUDES
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <errno.h>
+#include <stdbool.h>
+
+//DEFINES
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
+//DATA
+
 
 struct termios orig_termios;
+
+
+//TERMINAL
+
+
+void die(const char* s){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode(){
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode(){
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
 
-    tcgetattr(STDIN_FILENO, &raw);
 
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+
+
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+}
+
+
+//INIT
+
 
 int main(){
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    if(iscntrl(c)){
-        printf("%d\n", c);
+    while(true){
+        editorRefreshScreen();
+        editorProcessKeyPress();
     }
-    else{
-        printf("%d ('%c')\n", c, c);
-    }
+    
 
     return 0;
 }
