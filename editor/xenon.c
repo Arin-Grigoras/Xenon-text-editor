5cffbbfee293973bdbff63795a9b2fdc99571058
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-11 09:46:23 +0200

    MORE KEYS!!!

--- editor/xenon.c
+++ editor/xenon.c
@@ -19,10 +19,12 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
-
 
 //DATA
 
@@ -90,8 +92,13 @@ int editorReadKey() {
             if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
             if (seq[2] == '~') {
             switch (seq[1]) {
+                case '1': return HOME_KEY;
+                case '3': return DEL_KEY;
+                case '4': return END_KEY;
                 case '5': return PAGE_UP;
                 case '6': return PAGE_DOWN;
+                case '7': return HOME_KEY;
+                case '8': return END_KEY;
             }
             }
         } else {
@@ -100,7 +107,14 @@ int editorReadKey() {
             case 'B': return ARROW_DOWN;
             case 'C': return ARROW_RIGHT;
             case 'D': return ARROW_LEFT;
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
             }
+        }
+        } else if (seq[0] == 'O') {
+        switch (seq[1]) {
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
         }
         }
         return '\x1b';
@@ -250,6 +264,7 @@ void editorMoveCursor(int key) {
 }
 
 
+
 void editorProcessKeypress() {
     int c = editorReadKey();
     switch (c) {
@@ -257,6 +272,20 @@ void editorProcessKeypress() {
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
+        break;
+        case HOME_KEY:
+        E.cx = 0;
+        break;
+        case END_KEY:
+        E.cx = E.screencols - 1;
+        break;
+        case PAGE_UP:
+        case PAGE_DOWN:
+        {
+            int times = E.screenrows;
+            while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
         break;
         case ARROW_UP:
         case ARROW_DOWN:
@@ -266,7 +295,6 @@ void editorProcessKeypress() {
         break;
     }
 }
-
 
 //INIT
 
